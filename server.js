e9395a333f6535f62c20f5b52b0c1965cef0bb12
noor5141 <noorrockz007@outlook.com>
2017-02-14 22:33:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req,res){
-   res.sendFile('what is wrong with you bro'); 
+   res.send('what is wrong with you bro'); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
